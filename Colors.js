6c3747a37e1c89fce142555176277d5baa01846b
ShizraAzhar <shizraazhar104@gmail.com>
2025-05-15 00:29:57 +0500

    changing background theme

--- Colors.js
+++ Colors.js
@@ -2,7 +2,7 @@ export default {
     primary: '#2c3e50',      
     secondary: '#16a085',    
     accent: '#f39c12',       
-    backgroundTop: '#e0f7fa',   
+    backgroundTop: '#black',   
     backgroundBottom: '#ffffff', 
     text: '#34495e',
     white: '#fff',
